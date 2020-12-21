eea2346e8669bdf6059a6e7238f888ea5a510b06
Krystian Kalita <krystian.kalita@zetosoftware.pl>
2020-12-21 15:12:09 +0100

    test

--- app.ts
+++ app.ts
@@ -2,7 +2,6 @@ import express from "express";
 import morgan from "morgan";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import IRoute from "./interfaces/IRoute";
 import Passport from "./passport";
 
@@ -22,7 +21,12 @@ class App {
   private initializeMiddlewares() {
     this.app.use(morgan("dev"));
 
-    this.app.use(cors({ exposedHeaders: ["Set-Cookie"] }));
+    // eslint-disable-next-line func-names
+    this.app.use(function (req, res, next) {
+      res.header("Access-Control-Allow-Origin", "https://fejku.github.io"); // update to match the domain you will make the request from
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
     this.app.use(cookieParser());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
