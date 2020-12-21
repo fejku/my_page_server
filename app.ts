c2372afcb468d8cb71e2a8421fba9a9812026938
Krystian Kalita <krystian.kalita@zetosoftware.pl>
2020-12-21 22:45:45 +0100

    asd

--- app.ts
+++ app.ts
@@ -21,8 +21,10 @@ class App {
   private initializeMiddlewares() {
     this.app.use(morgan("dev"));
 
+    this.app.use(cookieParser());
+
     // eslint-disable-next-line func-names
-    this.app.use(function (req, res, next) {
+    this.app.use(function (req: express.Request, res: express.Response, next) {
       res.header("Access-Control-Allow-Origin", "https://fejku.github.io"); // update to match the domain you will make the request from
       // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
@@ -31,7 +33,7 @@ class App {
       res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
       next();
     });
-    this.app.use(cookieParser());
+
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
     Passport.passportLocalMiddleware();
