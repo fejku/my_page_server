68148f0f6646e3452874b759fcd6cafce30701c2
Krystian Kalita <krystian.kalita@zetosoftware.pl>
2020-12-21 23:35:11 +0100

    tete

--- app.ts
+++ app.ts
@@ -26,7 +26,7 @@ class App {
       res.header("Access-Control-Allow-Origin", "https://fejku.github.io"); // update to match the domain you will make the request from
       // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-      res.header("Access-Control-Allow-Headers", "*");
+      res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
       res.header("Access-Control-Allow-Credentials", "true");
       res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
       next();
